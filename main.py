b3fc6e1c2cf3fe4986ac60d87e9f99d16a50b52b
EmicoBinsfinder <Laugh56@>
2023-02-26 16:47:19 +0000

    Update

--- main.py
+++ main.py
@@ -6,20 +6,18 @@ LinkDataframe = pd.DataFrame(columns=['Link', 'Section'])
 
 for S in Section:
     #### Load in Datasets from the large text files
-    with open(f'C:/Users/eeo21/Documents/Startup_Datasets/CPC_Classifications_List/cpc-section-{S}_20220801.txt') as f:
-        lines = f.readlines()
+c        lines = f.readlines()
 
     for line in lines:
         Line_Elements = line.split("\t")
         ClassCode = Line_Elements[0]
         if len(ClassCode) <= 4:
             continue
-        link = f'https://patents.google.com/xhr/query?url=q%3D{ClassCode}%26oq%3D{ClassCode}&exp=&download=true'
+        link = f'https://patents.google.com/xhr/query?url=q%3D{ClassCode}%26oq%3D{ClassCode}"&"exp="&"download=true'
         entry = [link, ClassCode]
         LinkDataframe.loc[len(LinkDataframe)] = entry
-        print(entry)
 
-LinkDataframe.to_csv('Desktop')
+LinkDataframe.to_csv('/LinkCSV.csv')
 
 
 
